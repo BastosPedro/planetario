fb428c8cd62dc17b4a706efed56daea7a4af82ac
BastosPedro <pedro.bastos@ctec.ufal.br>
2019-05-14 21:46:47 -0300

    arrumei a main
    não é a versão final dela

--- main.py
+++ main.py
@@ -10,12 +10,29 @@ from reader import Data
 from plotter import Plotter
 from calculator import EulerModule
 
-dadosTeste = Data("2planetasXY.txt")
 
-eulerTeste = EulerModule(dadosTeste)
+def main():
+    print("Bem vindo!")
 
-eulerTeste.spreadSheet("mdsmeajude.xlsx")
+    dadosTeste = Data(input("Escreva o nome do arquivo (com seu diretório, se necessário) a ser lido: "))
 
-plotagem = Plotter("mdsmeajude.xlsx")
+    print("Fazendo cálculos, espere...\n")
+    eulerTeste = EulerModule(dadosTeste)
+    print("Cálculos concluídos!\n")
 
-plotagem.plot2d(["blue", "orange"], "teste.pdf")
+
+    sheetName = input("Escreva o nome da planilha (formato .xlsx, com seu diretório se necessário) a ser gravado: ")
+    if ".xlsx" not in sheetName:
+        raise Exception("Formato errado, tem que ser .xlsx!")
+    else:
+        eulerTeste.spreadSheet(sheetName)
+        print("Os dados foram gravados numa planilha!\n")
+
+    plotagem = Plotter(sheetName)
+
+    plotagem.plot2d(["blue", "orange"], input("Escreva o nome da imagem (com seu diretório, se necessário) a ser gravada: "))
+    print("Plotagem concluída com sucesso!\n")
+    
+    return
+
+main()
